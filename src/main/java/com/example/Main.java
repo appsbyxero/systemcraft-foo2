857d3d49246a563fb9c0b4c8df44db59dd23b51c
appsbyxero <appxero@gmail.com>
2024-08-25 12:28:27 +0530

    Provide starter code 1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,17 @@
 package com.example;
 
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        // Put your code here
+        final var pattern = args[0];
+        final var patternRegex = Pattern.compile(pattern);
+        final var path = args[1];
+        
+        try {
+            GrepMatcher.MatchInFile(path, patternRegex);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 }
